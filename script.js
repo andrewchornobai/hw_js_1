cf971b51d7d7bcc274a69ab58f41d1d79f08a02c
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-01 10:44:07 +0200

    1.JS

---
+++ script.js
@@ -0,0 +1,86 @@
+// alert("Hello world");
+
+// number => 20, 20.1;
+// string => 'some text',"some text", `some text`
+// boolean => true or false                             перші 6-сть примітивни тип даних
+// undifened
+// null
+// symbol
+// object => object, array                              не примітивний тип даних
+
+// es6+ => 2015
+
+//var,let,const
+
+var a = "atata";
+let b = "Barsa" //string
+const c = false; //boolean
+let d; //undefined
+let e = null; //null
+let f = Symbol();
+
+let arr = [1,"sara",[2,2]];
+let obj={
+    userName: 'Pavlo',
+    userSurname:'Stepanenko'
+};
+
+// let num1 = 3;
+// let num2 = num1;
+// console.log(num2);
+// num2 = 3;
+// console.log(num1);
+
+// let str1 = 'Hello';
+// let str2 = 'world';
+// let massage = str1+" "+ str2;
+// console.log(massage);
+
+// let n1 = 10.5;
+// let n2 = 5;
+// let n3 = "px";
+
+// console.log(n1, typeof n1); // окрім + JS приводить всі числа до типу даних number
+// let result = parseInt(n1);
+// console.log(result);
+// result = parseFloat(n1);
+// console.log(result);
+
+// let mess = confirm('some text');
+// let mess2 = prompt("enter some text");
+
+let name = prompt ("Enter your name","Petro");
+console.log(`My name is ${name}`);
+
+document.write("Hello world");
+
+
+
+// c = true;
+// console.log(c);
+
+
+
+// alert(a);
+// console.log(a);
+// a = 2000;
+// console.log(a);
+// a = a + 500;
+// console.log(a);
+// a = 'some text';
+// console.log(a);
+// a = 10;
+// console.log(a);
+// b = 20;
+// console.log(b);
+
+// {
+//     let t = 10; 
+// }
+// console.log(t);
+
+// const name = "Liza";
+
+// let userPassword = '123sds123';
+// user_password
+// 
